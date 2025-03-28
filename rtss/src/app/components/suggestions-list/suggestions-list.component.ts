bd8a725b915b1bdb36123dccc10a824e5a5dd431
Yashika-Shetty <yashika.shetty_19@sakec.ac.in>
2025-03-28 23:58:25 +0530

    Added components and services

---
+++ rtss/src/app/components/suggestions-list/suggestions-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suggestions-list',
+  imports: [],
+  templateUrl: './suggestions-list.component.html',
+  styleUrl: './suggestions-list.component.scss'
+})
+export class SuggestionsListComponent {
+
+}
